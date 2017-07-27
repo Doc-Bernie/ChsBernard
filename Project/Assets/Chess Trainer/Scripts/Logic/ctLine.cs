b692dc6442dcdc49b77ab3e6309122c68215bfe2
Jin Q S <fanzhi_2015@hotmail.com>
2017-07-27 16:47:18 +0800

    no message

--- Project/Assets/Chess Trainer/Scripts/Logic/ctLine.cs
+++ Project/Assets/Chess Trainer/Scripts/Logic/ctLine.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ctLine  {
-    public ctLibrary parent { get; set; }
+    public ctLibrary parent { get; private set; }
 
     public string name { get; set; }
 
-    public ctMove moves { get; set; }
+    public string moves ;
 
-    public ctLine(ctLibrary _parent)
+    public List<cgSimpleMove> moveList = new List<cgSimpleMove>();
+
+    public ctLine(ctLibrary _parent, string _name, string _moves = "")
     {
         parent = _parent;
-        moves = new ctMove(this);
+        name = _name;
+        moves = _moves;
+
+
+        cgNotation not = new cgNotation(new cgBoard());
+        not.Read(moves);
+
+        for (int i = 0; i < not.moves.Count; i++)
+            moveList.Add(not.moves[i]);
     }
 
     public void Clone(ctLine _line)
     {
         this.name = _line.name;
-        this.moves.Clone(_line.moves);
+        this.moves = _line.moves;
+
+        cgNotation not = new cgNotation(new cgBoard());
+        not.Read(this.moves);
     }
 
 }
